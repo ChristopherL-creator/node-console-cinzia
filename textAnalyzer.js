a69a9d5b78cacb8f9e979ae6aa63027974d10391
ChristopherL <101825137+ChristopherL-creator@users.noreply.github.com>
2022-05-12 17:41:49 +0200

    Update textAnalyzer.js

--- textAnalyzer.js
+++ textAnalyzer.js
@@ -1,23 +1,25 @@
 /////// STEP 1 - IMPORTARE FS
-/////// STEP 2 - LEGGERE GLI ARGOMENTI INSERITI DA CONSOLE
-/////// STEP 3 - PRENDERE IL PRIMO ELEMENTO COME "INPUTURL" E IL SECONDO COME "OUTPUTURL" (NON OBBLIGATORIO)
-/////// STEP 4 - LEGGERE IL CONTENUTO DEL FILE E LOGGARE IL NUMERO DI CARATTERI (SPAZI COMPRESI) E IL NUMERO DI CARATTERI (SPAZI ESCLUSI)
-/////// STEP 5 - SE L'UTENTE HA INSERITO L'"OUTPUTURL", SCRIVERE UN NUOVO FILE CON IL TESTO DELL'ORIGINALE PIU' I DATI DELL'ANALISI
-///////          SE L'UTENTE NON HA INSERITO L'"OUTPUTURL", SOVRASCRIVERE IL FILE ORIGINALE.
-/////// ESEMPIO: (ORIGINALE)"VIVA IL CSS!" (COPIA)"VIVA IL CSS!" \ NUMERO DI CARATTERI (SPAZI INCLUSI): 12 \ NUMERO DI CARATTERI (SPAZI ESCLUSI): 10
 
 const fs = require('fs');
 const myArgs = process.argv.slice(2);
 
+/////// STEP 2 - LEGGERE GLI ARGOMENTI INSERITI DA CONSOLE
 
 if (myArgs[0] === undefined) {
   console.error('mi serve il file di input')
   process.exit();
 }
 
+/////// STEP 3 - PRENDERE IL PRIMO ELEMENTO COME "INPUTURL" E IL SECONDO COME "OUTPUTURL" (NON OBBLIGATORIO)
+
 const inputUrl = myArgs[0];
 
 let outputUrl
+
+/////// STEP 4 - LEGGERE IL CONTENUTO DEL FILE E LOGGARE IL NUMERO DI CARATTERI (SPAZI COMPRESI) E IL NUMERO DI CARATTERI (SPAZI ESCLUSI)
+/////// STEP 5 - SE L'UTENTE HA INSERITO L'"OUTPUTURL", SCRIVERE UN NUOVO FILE CON IL TESTO DELL'ORIGINALE PIU' I DATI DELL'ANALISI
+///////          SE L'UTENTE NON HA INSERITO L'"OUTPUTURL", SOVRASCRIVERE IL FILE ORIGINALE.
+/////// ESEMPIO: (ORIGINALE)"VIVA IL CSS!" (COPIA)"VIVA IL CSS!" \ NUMERO DI CARATTERI (SPAZI INCLUSI): 12 \ NUMERO DI CARATTERI (SPAZI ESCLUSI): 10 
 
 if (myArgs[1] === undefined){
   outputUrl = inputUrl;
@@ -42,7 +44,7 @@ const inputLenghtWithoutSpaces = data.replace(/\s+/g, '').length;
 // console.log(inputLenghtWithoutSpaces);
 
 
-const result = data + '\r\n' + '\r\n' + 'Numero caratteri (spazi compresi): ' + inputLenghtWithSpaces + '\r\n' + '\r\n' + 'Numero caratteri (spazi esclusi): ' + + inputLenghtWithoutSpaces;
+const result = data + '\r\n' + '\r\n' + 'Numero caratteri (spazi compresi): ' + inputLenghtWithSpaces + '\r\n' + '\r\n' + 'Numero caratteri (spazi esclusi): ' + inputLenghtWithoutSpaces;
 
 try {
   fs.writeFileSync(outputUrl, result);
